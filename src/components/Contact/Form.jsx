f5f2b4460d402c010e981ebcbe245043303ad561
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-04-04 19:18:04 +0200

    Patch Form tags

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -2,10 +2,7 @@ export default function Form() {
 
     return (
 
-        <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field" onSubmit={ (e) => {
-            e.preventDefault()
-            alert('Form Send')
-            }}>
+        <form name="contact" method="post" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <input type="email" name="email" id="email" placeholder="Mail" />
             <input type="submit" value="SUBMIT" id="submit"/>
