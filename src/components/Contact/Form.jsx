fd89805fb3cf5864f3356fa34b77ccf390678c60
Jules BRUZEAU <jules.bruzeau@efrei.net>
2022-04-04 17:40:35 +0200

    Contact Section

---
+++ src/components/Contact/Form.jsx
@@ -0,0 +1,12 @@
+export default function Form() {
+
+    return (
+
+        <form action="POST" netlify>
+            <input type="email" name="email" id="email" placeholder="Mail" />
+            <input type="submit" value="SUBMIT" id="submit"/>
+        </form>
+
+    )
+
+}
---
+++ src/view/Contact.jsx
@@ -0,0 +1,26 @@
+import '../styles/contact.scss'
+
+import Form from '../components/Contact/Form'
+
+function Contact() {
+
+    return (
+
+        <section>
+
+            <div className="title">
+                <h1>Contact</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit quibusdam nihil necessitatibus cupiditate</p>
+            </div>
+
+            <div className="form-container">
+                <Form />
+            </div>
+
+        </section>
+
+    )
+
+}
+
+export default Contact
